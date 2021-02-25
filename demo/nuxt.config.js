20cb0c7b7fbc5df4bc00d73098927a46effa79b5
Pooya Parsa <pyapar@gmail.com>
2021-02-25 22:51:02 +0100

    chore: add codesandbox demo

---
+++ demo/nuxt.config.js
@@ -0,0 +1,5 @@
+export default {
+  buildModules: [
+    'nuxt-vite'
+  ]
+}
---
+++ demo/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <img class="logo" src="../assets/vite.svg">
+    <img class="logo" src="../assets/nuxt.svg">
+    <br>
+    <h1>Nuxt + Vite</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+}
+
+.logo {
+  width: 200px;
+  height: 200px;
+  display: inline-block;
+}
+</style>
