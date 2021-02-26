bcbec5734250b92c15b9cd53bcaf6e48a968a950
Pooya Parsa <pyapar@gmail.com>
2021-02-26 13:30:58 +0100

    try windicss

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,12 +1,20 @@
+import WindiCSS from 'vite-plugin-windicss'
 import viteModule from '../../src'
+
 export default {
   components: true,
+  vite: {
+    plugins: [
+      WindiCSS()
+    ]
+  },
   buildModules: [
     viteModule
   ],
   plugins: [
     '~/plugins/hello.js',
-    '~/plugins/no-export.js'
+    '~/plugins/no-export.js',
+    '~/plugins/windi.js'
   ],
   hooks: {
     'vue-renderer:context' (ssrContext) {
