26af7d20bd4f5f3cb6fe2401a9b385baa95e544d
Pooya Parsa <pyapar@gmail.com>
2021-02-26 10:45:33 +0100

    add enforce: pre

--- src/plugins/default-export.ts
+++ src/plugins/default-export.ts
@@ -3,6 +3,7 @@ import type { Plugin } from 'vite'
 export function defaultExportPlugin (getIds: () => string[]) {
   return <Plugin>{
     name: 'default-export-plugin',
+    enforce: 'pre',
     transform (code, id) {
       if (getIds().includes(id) || id.startsWith('nuxt_plugin_')) {
         if (!code.includes('export default')) {
