3ce2398ea2a16fe51c9e86f8248213bc0300de64
Pooya Parsa <pyapar@gmail.com>
2021-02-27 22:09:04 +0100

    add back test!

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -3,19 +3,19 @@
     <h2>Hello Vite from Nuxt2!</h2>
     <p><Foobar /></p>
     <pre v-text="stateText" />
-    <!-- <p>{{ res }}</p> -->
+    <p>{{ res }}</p>
   </div>
 </template>
 
 <script>
 export default {
   middleware: 'test-middleware',
-  // async asyncData ({ $http }) {
-  //   const res = await $http.$get('/api/test')
-  //   return {
-  //     res
-  //   }
-  // },
+  async asyncData ({ $http }) {
+    const res = await $http.$get('/api/test')
+    return {
+      res
+    }
+  },
   computed: {
     stateText () {
       return JSON.stringify(this.$store.state, null, 2).replace(/"/g, '')
