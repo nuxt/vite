74639a65725216257d399aa65d88b5395e9250ff
Pooya Parsa <pyapar@gmail.com>
2021-03-02 15:23:01 +0100

    fix: use mkdirpSync for client dir

--- src/plugins/cache-dir.ts
+++ src/plugins/cache-dir.ts
@@ -1,15 +1,15 @@
 import type { Plugin } from 'vite'
 import { resolve } from 'upath'
-import { mkdirp } from 'fs-extra'
+import { mkdirpSync } from 'fs-extra'
 
 export function cacheDirPlugin (rootDir, name: string) {
   const optimizeCacheDir = resolve(rootDir, 'node_modules', '.cache/vite', name)
+  mkdirpSync(resolve(optimizeCacheDir, 'temp'))
   return <Plugin> {
     name: 'nuxt:workaround-cache-dir',
-    async configResolved (resolvedConfig) {
+    configResolved (resolvedConfig) {
       // @ts-ignore
       resolvedConfig.optimizeCacheDir = optimizeCacheDir
-      await mkdirp(resolve(optimizeCacheDir, 'temp'))
     }
   }
 }
