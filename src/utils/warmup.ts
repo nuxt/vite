d249e5032b64b331f6c2e17668574569272ec086
Pooya Parsa <pyapar@gmail.com>
2021-03-13 13:36:13 +0100

    fix: properly use moduleGraph

--- src/utils/warmup.ts
+++ src/utils/warmup.ts
@@ -4,15 +4,11 @@ export async function warmupViteServer (server: ViteDevServer, entries: string[]
   const warmedUrls = new Set<String>()
 
   const warmup = async (url: string) => {
-    const script = await server.transformRequest(url, { html: false, ssr: true })
-    if (!script || typeof script === 'string' || !script.deps) {
-      return
-    }
-    await Promise.all((script.deps).map((url) => {
-      if (warmedUrls.has(url)) { return undefined }
-      warmedUrls.add(url)
-      return warmup(url)
-    }))
+    if (warmedUrls.has(url)) { return undefined }
+    warmedUrls.add(url)
+    await server.transformRequest(url)
+    const deps = Array.from(server.moduleGraph.urlToModuleMap.get(url).importedModules)
+    await Promise.all(deps.map(m => warmup(m.url)))
   }
 
   await Promise.all(entries.map(entry => warmup(entry)))
