d9fe2b2c3cebc9722dccc9785de96b827bc509a7
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-21 00:15:12 +0200

    :wrench: (docus) add docus config, move data from nuxt, remove settings

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -2,11 +2,6 @@ import { withDocus } from 'docus'
 
 export default withDocus({
   buildModules: ['vue-plausible'],
-  docus: {
-    colors: {
-      primary: '#ffcb23'
-    }
-  },
   plausible: {
     domain: 'vite.nuxtjs.org'
   }
