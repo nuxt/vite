5e4f948f31601fcf9d087305028db415035fd29f
Yaël Guilloux <yael.guilloux@gmail.com>
2021-05-22 16:35:52 +0200

    docs: update docus (#145)

---
+++ docs/docus.config.js
@@ -0,0 +1,16 @@
+export default {
+  title: '⚡ Nuxt Vite',
+  url: 'https://vite.nuxtjs.org',
+  twitter: '@nuxt_js',
+  github: {
+    repo: 'nuxt/vite',
+    branch: 'main',
+    dir: 'docs',
+    releases: false
+  },
+  theme: {
+    colors: {
+      primary: '#ffcb23'
+    }
+  }
+}
