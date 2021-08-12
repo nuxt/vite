864f15617a313e78bbc2604fa95677f5411dcaa2
Iván Montes <drslump@pollinimini.net>
2021-08-12 09:56:27 +0200

    fix: hide warm-up errors (#168)

--- src/utils/warmup.ts
+++ src/utils/warmup.ts
@@ -1,4 +1,5 @@
 import type { ViteDevServer } from 'vite'
+import consola from 'consola'
 
 export async function warmupViteServer (server: ViteDevServer, entries: string[]) {
   const warmedUrls = new Set<String>()
@@ -6,7 +7,11 @@ export async function warmupViteServer (server: ViteDevServer, entries: string[]
   const warmup = async (url: string) => {
     if (warmedUrls.has(url)) { return undefined }
     warmedUrls.add(url)
-    await server.transformRequest(url)
+    try {
+      await server.transformRequest(url)
+    } catch (e) {
+      consola.debug('Warmup for %s failed with: %s', url, e)
+    }
     const deps = Array.from(server.moduleGraph.urlToModuleMap.get(url).importedModules)
     await Promise.all(deps.map(m => warmup(m.url)))
   }
